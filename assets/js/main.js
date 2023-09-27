e40dcb6e94e305d4176bbd188229bdccd808c490
Aryanstha <aryanstha4859@gmail.com>
2023-09-28 00:16:15 +0545

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -291,21 +291,3 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
-
-var chatbox = document.getElementById('fb-customer-chat');
-chatbox.setAttribute("page_id", "276427919892722");
-chatbox.setAttribute("attribution", "biz_inbox");
-  window.fbAsyncInit = function() {
-  FB.init({
-    xfbml            : true,
-    version          : 'v18.0'
-  });
-};
-
-  (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
